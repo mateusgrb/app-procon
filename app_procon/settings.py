b14cf37d8f9b6310e7cf4ce4d0503b681b5604c2
Mateus Gondim <gondim.mateus@gmail.com>
2013-01-30 17:00:13 -0300

    Updating settings.py

--- app_procon/settings.py
+++ app_procon/settings.py
@@ -13,9 +13,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'app_procon',#'/home/mateus/RepositorioProcon/app_procon/app_procon/sqlite3.db',  # Or path to database file if using sqlite3.
+        'NAME': 'app_procon',  # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'assenav',                  # Not used with sqlite3.
+        'PASSWORD': 'root',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
