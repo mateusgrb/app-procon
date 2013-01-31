aa3288efeb4a55af14b731b8a8e0f7b87d0feb63
Vanessa Gomes <vane.gomesl@gmail.com>
2013-01-31 18:42:12 -0300

    Updating get_evaluation_by_region function tu suit the new Models\' standard

--- companiesEvaluator/evaluators/regionsEvaluator.py
+++ companiesEvaluator/evaluators/regionsEvaluator.py
@@ -1,18 +1,18 @@
 # -*- coding:utf-8 -
 # This Python file uses th encoding utf-8
 
-from companiesEvaluator.models import Reclamacao
+from companiesEvaluator.models import Reclamacao, Fornecedor
 
 #This file contains functions that evaluate a company according to the Region complaints.
 NORTE = 'Norte'
 NORDESTE = 'Nordeste'
 SUL = 'Sul'
-C_OESTE = 'Centro-Oeste'
+C_OESTE = 'Centro-oeste'
 SUDESTE = 'Sudeste'
 
 def get_evaluation_by_region(company):
     '''
-    This function gives the evaluation of a company based on the evaluation region.
+    This function gives the evaluation of a company (Fornecedor) based on the evaluation region.
     1. Filters the results based on the 'str_nome_fantasia' of the company.
     
     2. The return contains the amount of all claims of every year for the company.
@@ -23,13 +23,13 @@ def get_evaluation_by_region(company):
     Example: {'Norte'=100, 'Nordeste'=500, 'Centro-Oeste'=700, 'Sul'=1000, 'Sudeste'=3000}
     '''
     
-    #Esse filtro pode mudar.
-   
-    norte_evaluation = Reclamacao.objects.filter(str_nome_fantasia=company, regiao=NORTE)
-    nordeste_evaluation = Reclamacao.objects.filter(str_nome_fantasia=company, regiao=NORDESTE)
-    centro_oeste_evaluation = Reclamacao.objects.filter(str_nome_fantasia=company, regiao=C_OESTE)
-    sul_evaluation = Reclamacao.objects.filter(str_nome_fantasia=company, regiao=SUL)
-    sudeste_evaluation = Reclamacao.objects.filter(str_nome_fantasia=company, regiao=SUDESTE)
+    #Esse filtro pode mudar
+
+    norte_evaluation = Reclamacao.objects.filter(fornecedor__str_nome_fantasia=company, regiao=NORTE)
+    nordeste_evaluation = Reclamacao.objects.filter(fornecedor__str_nome_fantasia=company, regiao=NORDESTE)
+    centro_oeste_evaluation = Reclamacao.objects.filter(fornecedor__str_nome_fantasia=company, regiao=C_OESTE)
+    sul_evaluation = Reclamacao.objects.filter(fornecedor__str_nome_fantasia=company, regiao=SUL)
+    sudeste_evaluation = Reclamacao.objects.filter(fornecedor__str_nome_fantasia=company, regiao=SUDESTE)
     
     return {NORTE: len(norte_evaluation), NORDESTE: len(nordeste_evaluation), C_OESTE: len(centro_oeste_evaluation), SUL: len(sul_evaluation), SUDESTE: len(sudeste_evaluation)}
     
