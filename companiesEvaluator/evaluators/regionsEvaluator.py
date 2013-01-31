bc67878e7f7fe85dd182a4f695c901d2dcdd8bd3
Vanessa Gomes <vane.gomesl@gmail.com>
2013-01-31 18:54:46 -0300

    Updating get_evaluation_by_region_year function to suit the new Models\' standard

--- companiesEvaluator/evaluators/regionsEvaluator.py
+++ companiesEvaluator/evaluators/regionsEvaluator.py
@@ -32,10 +32,11 @@ def get_evaluation_by_region(company):
     sudeste_evaluation = Reclamacao.objects.filter(fornecedor__str_nome_fantasia=company, regiao=SUDESTE)
     
     return {NORTE: len(norte_evaluation), NORDESTE: len(nordeste_evaluation), C_OESTE: len(centro_oeste_evaluation), SUL: len(sul_evaluation), SUDESTE: len(sudeste_evaluation)}
+
     
 def get_evaluation_by_region_year(company, year):
     '''
-    This function gives the evaluation of a company based on the evaluation region and the specified year.
+    This function gives the evaluation of a company (Fornecedor) based on the evaluation region and the specified year.
     1. Filters the results based on the 'str_nome_fantasia' of the company.
     
     2. The return contains the amount of all claims of every year for the company.
@@ -48,16 +49,13 @@ def get_evaluation_by_region_year(company, year):
     Example: {'Norte'=100, 'Nordeste'=500, 'Centro-Oeste'=700, 'Sul'=1000, 'Sudeste'=3000}
     '''
     
-    #Esse filtro pode mudar.
-    norte_evaluation = Reclamacao.objects.filter(str_nome_fantasia=company, regiao=NORTE, ano_calendario=year)
-    nordeste_evaluation = Reclamacao.objects.filter(str_nome_fantasia=company, regiao=NORDESTE, ano_calendario=year)
-    centro_oeste_evaluation = Reclamacao.objects.filter(str_nome_fantasia=company, regiao=C_OESTE, ano_calendario=year)
-    sul_evaluation = Reclamacao.objects.filter(str_nome_fantasia=company, regiao=SUL, ano_calendario=year)
-    sudeste_evaluation = Reclamacao.objects.filter(str_nome_fantasia=company, regiao=SUDESTE, ano_calendario=year)
-    
+    #For now, the query search based on Fornecedors' str_nome_fantasia.
+
+    norte_evaluation = Reclamacao.objects.filter(fornecedor__str_nome_fantasia=company, regiao=NORTE, ano_calendario=year)
+    nordeste_evaluation = Reclamacao.objects.filter(fornecedor__str_nome_fantasia=company, regiao=NORDESTE, ano_calendario=year)
+    centro_oeste_evaluation = Reclamacao.objects.filter(fornecedor__str_nome_fantasia=company, regiao=C_OESTE, ano_calendario=year)
+    sul_evaluation = Reclamacao.objects.filter(fornecedor__str_nome_fantasia=company, regiao=SUL, ano_calendario=year)
+    sudeste_evaluation = Reclamacao.objects.filter(fornecedor__str_nome_fantasia=company, regiao=SUDESTE, ano_calendario=year)
+ 
     return {NORTE: len(norte_evaluation), NORDESTE: len(nordeste_evaluation), C_OESTE: len(centro_oeste_evaluation), SUL: len(sul_evaluation), SUDESTE: len(sudeste_evaluation)}
-
-
-#def get_evaluation_by_region_gender(company, gender)
-
     
