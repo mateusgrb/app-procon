dbed7a96fa0808bfcb67f72a6ed6ef80967621d8
Vanessa Gomes <vane.gomesl@gmail.com>
2013-02-08 19:22:47 -0300

    Adding unicode method on models.

--- companiesEvaluator/models.py
+++ companiesEvaluator/models.py
@@ -11,9 +11,15 @@ class Fornecedor(models.Model):
     nome_fantasia_rfb = models.CharField(max_length=180)
     cnae_principal = models.CharField(max_length=20)
     desc_cnae_principal = models.CharField(max_length=180)
+    
+    def __unicode__(self):
+        return 'Razao social: ' + str(self.str_razao_social) + '. Nome Fantasia: ' + str(self.str_nome_fantasia) + '. CNPJ: ' + str(self.numero_cnpj) + '. Radical: ' + str(self.radical_cnpj)
 
 class Regiao(models.Model):
     nome_regiao = models.CharField(max_length=20)
+    
+    def __unicode__(self):
+        return str(self.nome_regiao)
 
 class Reclamacao(models.Model):
     ano_calendario = models.IntegerField()
@@ -30,3 +36,6 @@ class Reclamacao(models.Model):
     cep_consumidor = models.CharField(max_length=20)
     regiao = models.ForeignKey(Regiao)
     fornecedor = models.ForeignKey(Fornecedor)
+    
+    def __unicode__(self):
+        return 'Ano: ' + str(self.ano_calendario) + '. Atendida: ' + str(self.atendida) + '. Regiao: <' + str(self.regiao) + '>. Fornecedor: <' + str(self.fornecedor) + '>.'
