8d5c94788b08a4e5e8ac5f6297510553d58a4c35
Mateus Gondim <gondim.mateus@gmail.com>
2013-02-08 17:26:54 -0300

    Models modified(Added Regiao)

--- companiesEvaluator/models.py
+++ companiesEvaluator/models.py
@@ -2,22 +2,23 @@
 from django.db import models
 
 class Fornecedor(models.Model):
-    str_razao_social = models.CharField(max_length=180, primary_key=True)
+    str_razao_social = models.CharField(max_length=180)
     str_nome_fantasia = models.CharField(max_length=180)
     tipo = models.IntegerField()
-    numero_cnpj = models.CharField(max_length=20)
+    numero_cnpj = models.CharField(max_length=20, unique=True)
     radical_cnpj = models.CharField(max_length=12)
     razao_social_rfb = models.CharField(max_length=180)
     nome_fantasia_rfb = models.CharField(max_length=180)
     cnae_principal = models.CharField(max_length=20)
     desc_cnae_principal = models.CharField(max_length=180)
 
+class Regiao(models.Model):
+    nome_regiao = models.CharField(max_length=20)
+
 class Reclamacao(models.Model):
     ano_calendario = models.IntegerField()
     data_arquivamento = models.DateTimeField()
     data_abertura = models.DateTimeField()
-    codigo_regiao = models.CharField(max_length=20)
-    regiao = models.CharField(max_length=20)
     uf = models.CharField(max_length=2)
     atendida = models.CharField(max_length=1)
     codigo_assunto = models.IntegerField()
@@ -27,4 +28,5 @@ class Reclamacao(models.Model):
     sexo_consumidor = models.CharField(max_length=1)
     faixa_etaria_consumidor = models.CharField(max_length=25)
     cep_consumidor = models.CharField(max_length=20)
+    regiao = models.ForeignKey(Regiao)
     fornecedor = models.ForeignKey(Fornecedor)
