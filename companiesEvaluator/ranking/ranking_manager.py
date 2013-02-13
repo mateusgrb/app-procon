56b98314752792aec14ac2b72efb9b83c47c6831
Mateus Gondim <gondim.mateus@gmail.com>
2013-02-13 20:25:10 -0300

    Changing function's name

--- companiesEvaluator/ranking/ranking_manager.py
+++ companiesEvaluator/ranking/ranking_manager.py
@@ -2,7 +2,7 @@
 from companiesEvaluator.models import Reclamacao, Fornecedor
 from django.db import connection
 
-def rank_by_complains(limit=20):
+def rank_by_complaints(limit=20):
     query = 'SELECT f.id, f.str_razao_social, f.radical_cnpj, (select count(r.id) FROM companiesEvaluator_reclamacao r where r.fornecedor_id = f.id) as qtd_reclamacoes FROM companiesEvaluator_fornecedor f group by f.radical_cnpj order by qtd_reclamacoes desc limit ' + str(limit)
     cursor = connection.cursor()
     cursor.execute(query)
